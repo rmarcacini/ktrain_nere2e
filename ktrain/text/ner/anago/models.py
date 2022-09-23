bb227e6d6e10d3a33039d78e76f9037d790ffb9f
rmarcacini <40037976+rmarcacini@users.noreply.github.com>
2022-09-23 16:55:00 -0300

    Update models.py

--- ktrain/text/ner/anago/models.py
+++ ktrain/text/ner/anago/models.py
@@ -160,6 +160,7 @@ class BiLSTMCRF(object):
             keras.layers.GRU(units=self._word_lstm_size, return_sequences=True)
         )(word_embeddings)
         z = keras.layers.MultiHeadAttention(num_heads=2, key_dim=2)(z,z)
+        z = keras.layers.Flatten()(z)
         z = keras.layers.Dense(self._fc_dim, activation="tanh")(z)
 
         if self._use_crf:
